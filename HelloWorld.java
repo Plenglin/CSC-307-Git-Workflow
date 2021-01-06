2477e5b8f58cd709b25d07b3b04736a11d6332b9
Astrid Yu <astrid@astrid.tech>
2021-01-06 10:27:36 -0800

    Add year

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello world, by Astrid Yu");
+        System.out.println("Hello world, by Astrid Yu (2nd year)");
     }
 }
