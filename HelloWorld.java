550bc2d62895281d3f53db973d8990221f34dab9
Astrid Yu <astrid@astrid.tech>
2021-01-06 10:31:07 -0800

    Another change because

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Goodbye Universe, by Astrid Yu (2nd year)");
+        System.out.println("Goodbye Universe, by Astrid Yu");
+        System.out.println("2nd year");
     }
 }
